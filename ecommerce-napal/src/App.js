86b49335c54b0f41c8202900f8becdf210e298fb
Luciano Signorelli <luchitosignorelli@gmail.com>
2021-08-02 19:35:31 -0300

    Add bootstrap y navBar

--- ecommerce-napal/src/App.js
+++ ecommerce-napal/src/App.js
@@ -4,7 +4,13 @@ import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <NavBar></NavBar>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-12">
+        <NavBar></NavBar>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- ecommerce-napal/src/components/NavBar.jsx
+++ ecommerce-napal/src/components/NavBar.jsx
@@ -1,13 +1,24 @@
-import logo from '../logo.svg';
-
 export default function navBar() {
+    const handleClick = (event) =>{
+        console.log(event);
+    }
     return (<>
-        <ul>
-  <li><a href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
-  <li><a href="#contact">Contact</a></li>
-  <li><a href="#about">About</a></li>
-</ul>
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+				 
+                 <button onClick={handleClick} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                     <span className="navbar-toggler-icon">NAPAL</span>
+                 </button> <a onClick={handleClick} className="navbar-brand" href="#">Napal</a>
+                 <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                     <ul className="navbar-nav">
+                         <li className="nav-item active">
+                              <a className="nav-link" href="#">Velas <span className="sr-only"></span></a>
+                         </li>
+                         <li className="nav-item">
+                              <a className="nav-link" href="#">Floreros</a>
+                         </li>
+                     </ul>
+                 </div>
+             </nav>
     </>)
 
 }
