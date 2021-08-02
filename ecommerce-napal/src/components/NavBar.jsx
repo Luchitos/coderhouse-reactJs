dba3fd505c0a22753c364b41be711eabfdf6450b
Luciano Signorelli <luchitosignorelli@gmail.com>
2021-08-02 19:21:32 -0300

    Add component and bootstrap

---
+++ ecommerce-napal/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import logo from '../logo.svg';
+
+export default function navBar() {
+    return (<>
+        <ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+    </>)
+
+}
