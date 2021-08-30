4b4ea58b28b4ff5355ded6f62cfedc853425f8d2
Luciano Signorelli <luchitosignorelli@gmail.com>
2021-08-30 11:16:38 -0300

    In progress ContextCart

---
+++ ecommerce-napal/src/context/cartContext.jsx
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+
+export const ContextCartCustom = React.createContext();
+
+export default function CartContext() {
+    return <>
+    
+    
+    </>
+}
