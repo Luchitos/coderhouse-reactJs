f3d27b9ac6e50c54c6b6ff6e8f1e1309f3e86009
Luciano Signorelli <luchitosignorelli@gmail.com>
2021-09-01 23:07:50 -0300

    config firebase

---
+++ ecommerce-napal/src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCHHVJlX0xU0n86oakNrf9KaWjbJKgROY",
+  authDomain: "reactjscoderhouse.firebaseapp.com",
+  projectId: "reactjscoderhouse",
+  storageBucket: "reactjscoderhouse.appspot.com",
+  messagingSenderId: "773259937537",
+  appId: "1:773259937537:web:1380f3ee15e011e9ae7d00"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
